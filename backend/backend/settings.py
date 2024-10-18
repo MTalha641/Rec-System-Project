b8573ef56f1c36a90f608a03262509c364f75d99
MTalha641 <talhakodvavi1@gmail.com>
2024-10-19 01:49:47 +0500

    seperated folders

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'backend',
+    'items',
+    'rentals',
+    'users'
+    
+    
+
 ]
 
 MIDDLEWARE = [
